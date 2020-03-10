b5b4e7be519cdc9a01a0f94e5d974bbf3a8df83a
echasnovski <evgeni.chasnovski@gmail.com>
2020-03-10 22:16:26 +0200

    Implement methods `coeffs()` and `_coeffs_by_ind()` of `rv_piecelin`.

--- randomvars/_rv_piecelin.py
+++ randomvars/_rv_piecelin.py
@@ -26,7 +26,54 @@ class rv_piecelin(rv_continuous):
         super(rv_piecelin, self).__init__(*args, **kwargs)
 
     def _pdf(self, x, *args):
+        """ Implementation of probability density function
+        """
         return np.interp(x, self._x, self._y)
+
+    def _coeffs_by_ind(self, ind):
+        """ Compute density coefficients based on index of interval.
+
+        Index `i` corresponds to coefficients from interval with endpoints
+        `self._x[i-1]` and `self._x[i]`. Which interval values of `self._x`
+        represent should be decided before calling this function during
+        computation of `ind`.  Indexes 0 and `len(self._x)` result in zero
+        coefficients.
+        """
+
+        inter = np.zeros_like(ind)
+        slope = np.zeros_like(ind)
+
+        ind_is_in = np.logical_and(ind > 0, ind < len(self._x))
+        inds_in = ind[ind_is_in]
+
+        slope[ind_is_in] = (self._y[inds_in - 1] - self._y[inds_in]) / (
+            self._x[inds_in - 1] - self._x[inds_in]
+        )
+        inter[ind_is_in] = (
+            self._y[inds_in - 1] - slope[ind_is_in] * self._x[inds_in - 1]
+        )
+
+        return (inter, slope)
+
+    def coeffs(self, x):
+        """ Compute density coefficients based on `x`.
+
+        For density x-grid elements coefficients are computed from the right
+        interval, except for last element, for which its left (i.e. last)
+        interval is used.
+
+        Returns
+        -------
+        coeffs : tuple of coefficients
+            Tuple with two arrays (both same length as `x`) representing
+            intercept and slope.
+        """
+
+        ind = np.searchsorted(self._x, x, side="right")
+        # Include right-most edge of density x-grid to return coefficients from first
+        # interval
+        ind[x == self._x[-1]] = len(self._x) - 1
+        return self._coeffs_by_ind(ind)
 
 
 def trapez_integral(x, y):
