a7e818c139c22bf7369dc15fc96358d3bde5fc5d
echasnovski <evgeni.chasnovski@gmail.com>
2020-03-17 20:56:37 +0200

    Update 'experiments/regrid.py'.

--- experiments/regrid.py
+++ experiments/regrid.py
@@ -64,9 +64,15 @@ def piecelin_quantiles(x, y, probs):
     return rv.ppf(probs)
 
 
-def regrid_curvature(x, y, n_grid):
+def regress_to_mean(x, y, alpha=0.5):
+    integral_mean = trapez_integral(x, y) / (x[-1] - x[0])
+    return (1 - alpha) * y + alpha * integral_mean
+
+
+def regrid_curvature(x, y, n_grid, alpha=0.5):
     curv = abs_curvature(x, y)
-    x_new = piecelin_quantiles(x, curv, np.linspace(0, 1, n_grid))
+    curv_regr = regress_to_mean(x, curv, alpha)
+    x_new = piecelin_quantiles(x, curv_regr, np.linspace(0, 1, n_grid))
     y_new = np.interp(x_new, x, y)
     return x_new, y_new
 
@@ -86,7 +92,7 @@ def regrid_quantile(x, y, n_grid):
 
 def dist_grid(grid_base, grid_new, method=None):
     if method is None:
-        method = "maxabs"
+        method = "meanabs"
 
     diff = np.interp(grid_base[0], grid_new[0], grid_new[1]) - grid_base[1]
 
@@ -108,16 +114,16 @@ def regrid_optimize(x, y, n_grid, maxiter=np.inf, fit_method=None):
     supp = tuple(x[[0, -1]])
 
     def fit_func(x_inn):
-        x_fit = np.r_(supp[0], x_inn, supp[1])
+        x_fit = np.concatenate(([supp[0]], x_inn, [supp[1]]))
         y_fit = np.interp(x_fit, x, y)
         return dist_grid((x, y), (x_fit, y_fit), method=fit_method)
 
-    x_init = np.linspace(x[0], x[-1], n_grid + 2)[1:-1]
-    bounds = [supp] * n_grid
+    x_init = np.linspace(x[0], x[-1], n_grid)[1:-1]
+    bounds = [supp] * (n_grid - 2)
     x_inn_res = minimize(
         fit_func, x_init, bounds=bounds, options={"maxiter": maxiter}
     ).x
-    x_res = np.r_(supp[0], x_inn_res, supp[1])
+    x_res = np.concatenate(([supp[0]], x_inn_res, [supp[1]]))
     y_res = np.interp(x_res, x, y)
 
     return x_res, y_res
@@ -163,16 +169,32 @@ dist_grid((x, y), (x_curv, y_curv), method="meanabs")
 dist_grid((x, y), (x_quant, y_quant), method="meanabs")
 dist_grid((x, y), (x_optim, y_optim), method="meanabs")
 
-# plotting regridding outputs
+trapez_integral(x_equi, y_equi)
+trapez_integral(x_curv, y_curv)
+trapez_integral(x_quant, y_quant)
+trapez_integral(x_optim, y_optim)
+
+# Plot regridding outputs
 fig, ax = plt.subplots()
 # ax.set_xlim([-7000, 7000])
 ax.plot(x, y)
 ax.plot(x_equi, y_equi, color="red", marker="o")
-# ax.plot(x_curv, y_curv, color="green")
+ax.plot(x_curv, y_curv, color="green", marker="o")
 # ax.plot(x_quant, y_quant)
 ax.plot(x_optim, y_optim, color="blue", marker="o")
 
-# Plotting absolute curvature
+# Plot absolute and regressed-to-mean curvatures
+abs_curv = abs_curvature(x, y)
+abs_curv_regr = regress_to_mean(x, abs_curv, alpha=0.5)
+
 fig, ax = plt.subplots()
 ax.plot(x, y)
-ax.plot(x, abs_curvature(x, y))
+ax.plot(x, abs_curv, color="blue")
+ax.plot(x, abs_curv_regr, color="green")
+
+# Study effect of "regress-to-mean" parameter on approximation quality
+alpha_grid = np.linspace(0, 1, 11)
+[
+    dist_grid((x, y), regrid_curvature(x, y, n_grid_new, alpha=a), method="maxabs")
+    for a in alpha_grid
+]
