d4d2c229565e216a1169c783ec05142538d4bc2a
echasnovski <evgeni.chasnovski@gmail.com>
2020-03-27 16:12:12 +0200

    Update 'experiments.py' with faster implementation of `regrid_maxtol()`.

--- experiments/regrid.py
+++ experiments/regrid.py
@@ -9,109 +9,107 @@ from rv_piecelin import rv_piecelin
 
 
 #%% Functions
-def coeffs_from_points(point1, point2):
-    slope = (point2[1] - point1[1]) / (point2[0] - point1[0])
-    inter = point1[1] - slope * point1[0]
-    return inter, slope
-
-
-def line_segment_intersection(point, slope, seg_point_1, seg_point_2):
-    """Return the (smallest) intersection between line and segment
-
-    Line is defined as `y = slope*(x-point[0])+point[1]`, i.e. passing through
-    point `point` and with slope `slope`. Segment is defined as connecting
-    points `seg_point_1` and `seg_point_2`. **Note** that it is assumed that
-    `seg_point_1[0] < seg_point_2[0]` (segment points are given in increasing
-    order of x-coordinate).
-
-    Returns
-    -------
-    point : Tuple representing point of intersection. `None` if no
-    intersection.
-    """
-    # Compute parameters of line and segment's line in form `y=a*x+b`
-    a1 = slope
-    b1 = point[1] - slope * point[0]
-    b2, a2 = coeffs_from_points(seg_point_1, seg_point_2)
-
-    if a1 == a2:
-        if b1 == b2:
-            return seg_point_1[0], seg_point_1[1]
-        else:
-            return None
-    else:
-        x_inter = (b2 - b1) / (a1 - a2)
-        if seg_point_1[0] <= x_inter <= seg_point_2[0]:
-            return x_inter, a1 * x_inter + b1
-        else:
-            return None
-
-
-def coneedges_segment_intersection(xy_base, slope_window, seg_point_1, seg_point_2):
+# All functions related to `regrid_maxtol()` are written using tuples for
+# function arguments as much as possible to increase execution speed.
+# This showed significant increase for most tupical cases (~10%).
+def coneedges_segment_intersection(
+    base_x, base_y, slope_min, slope_max, seg1_x, seg1_y, seg2_x, seg2_y
+):
     """Compute intersection of segment and 2d cone edges
 
-    Two-dimensional cone is defined as all rays from point `xy_base` and  with
-    slopes inside `slope_window` range (rays are directed to the right of
-    `xy_base`).
+    Two-dimensional cone is defined as all rays from point `(base_x, base_y)`
+    and  with slopes inside `[slope_min, slope_max]` range (rays are directed
+    to the right of origin). Segment connects point `(seg1_x, seg1_y)` and
+    `(seg2_x, seg2_y)`.
 
     This function computes if there is an intersection between segment and
-    both edges of this cone. In other words, it computes possible intersection
-    points between segment and two lines:
-    `slope_window[0]*(x-xy_base[0])+xy_base[1]` and
-    `slope_window[1]*(x-xy_base[0])+xy_base[1]`.
-    
+    some edge of the cone. In other words, it computes possible intersection
+    points between segment and two lines: `slope_min*(x-base_x)+base_y` and
+    `slope_max*(x-base_x)+base_y`.
+
+    **Note** that for correctness of algorithm, it is assumed that `(seg1_x,
+    seg1_y)` is strictly inside the cone (not on edge). In other words, slope
+    of line through `(base_x, base_y)` and `(seg1_x, seg1_y)` lies strictly
+    inside `(slope_min, slope_max)` interval.  This enables using simplified
+    algorithm for computing if there is an intersection (which terminates early
+    if there is no intersection, which in turn increases execution speed) and
+    its coordinates. For example, it implies that there can be only one
+    intersection between segment and cone edges.
+
     Parameters
     ----------
-    xy_base : Tuple with two numbers
-        Represents origin point of 2d cone.
-    slope_window : Tuple with two numbers (first is less than second)
-    seg_point_1 : Tuple with two numbers
-        Represents start of segment.
-    seg_point_2 : Tuple with two numbers
-        Represents end of segment.
-    
+    base_x, base_y : Numbers for x and y coordinates of 2d cone origin point
+    slope_min, slope_max : Numbers for minimum and maximum values of slope
+    (edges of 2d cone)
+    seg1_x, seg1_y : Numbers for x and y coordinates of segment start
+    seg2_x, seg2_y : Numbers for x and y coordinates of segment end
+
     Returns
     -------
-    points : Tuple with two elements.
-        Elements might be either tuple with two numbers (if there is
-        intersection) or `None` (otherwise).
+    point : Tuple with two elements (if there is intersection) or `None`
+    (otherwise).
     """
-    return tuple(
-        line_segment_intersection(xy_base, slope, seg_point_1, seg_point_2)
-        for slope in slope_window
-    )
+    seg_slope_1 = (seg1_y - base_y) / (seg1_x - base_x)
+    seg_slope_2 = (seg2_y - base_y) / (seg2_x - base_x)
+
+    # This part uses assumption that `seg_slope_1` lies inside `(slope_min,
+    # slope_max)`. It enables easy checking if cone edge intersects segment by
+    # comparing edge's slope with segment's slopes.
+    if (seg_slope_1 <= slope_min) or (seg_slope_2 <= slope_min):
+        a1 = slope_min
+    elif (seg_slope_1 >= slope_max) or (seg_slope_2 >= slope_max):
+        a1 = slope_max
+    else:
+        return None
+
+    # This part executes only if there is an intersection between some cone
+    # edge (with base slope `a1`) and segment. Thus there is no need to check
+    # if intersection of lines lie inside segment.
+    b1 = base_y - a1 * base_x
+    a2 = (seg2_y - seg1_y) / (seg2_x - seg1_x)
+    b2 = seg1_y - a2 * seg1_x
+
+    x_res = (b2 - b1) / (a1 - a2)
+    y_res = a2 * x_res + b2
+    return x_res, y_res
 
 
-def tolerance_slope_window(xy_base, point, tol):
+def tolerance_slope_window(base_x, base_y, point_x, point_y, tol):
     """ Compute slope window for rays to be within tolerance of supplied point
 
-    Computes slope window of 2d cone with base point `xy_base` and which passes
-    through points `(point[0], point[1]-tol)` and `(point[0], point[1]+tol)`.
+    Computes slope window of 2d cone with base point `(base_x, base_y)` and
+    which passes through points `(point_x, point_y-tol)` and `(point_x,
+    point_y+tol)`.
     """
-    return tuple(
-        (point[1] - xy_base[1] + extra) / (point[0] - xy_base[0])
-        for extra in [-tol, tol]
-    )
+    slope_min = (point_y - base_y - tol) / (point_x - base_x)
+    slope_max = (point_y - base_y + tol) / (point_x - base_x)
+
+    return slope_min, slope_max
 
 
-def intersect_inervals(interval1, interval2):
-    return max(interval1[0], interval2[0]), min(interval1[1], interval2[1])
+def intersect_inervals(inter1_min, inter1_max, inter2_min, inter2_max):
+    """Compute intersection of intervals
+
+    Computes intersections of intervals `(inter1_min, inter1_max)` and
+    `(inter2_min, inter2_max)`.
+    """
+    return max(inter1_min, inter2_min), min(inter1_max, inter2_max)
 
 
 def regrid_maxtol(x, y, tol=1e-3):
     """Regrid with maximum tolerance
 
     Regrid input xy-greed so that maximum difference between points on output
-    piecewise-linear function and xy-greed is less than `tol` (currently it is
-    always equal to `tol`).
-    
+    piecewise-linear function and input xy-greed is not more than `tol`
+    (currently it is always equal to `tol`).
+
     Parameters
     ----------
     x : Numpy numeric array.
     y : Numpy numeric array.
     tol : Single number, optional
         Tolerance, by default 1e-3
-    
+
     Returns
     -------
     xy_grid : Tuple with two numpy numeric arrays with same lengths
@@ -119,44 +117,56 @@ def regrid_maxtol(x, y, tol=1e-3):
     if len(x) <= 2:
         return x, y
 
-    x_res, y_res = [np.empty_like(i, dtype=np.float64) for i in [x, y]]
-
-    # First point should also be in result
-    x_res[0], y_res[0] = x[0], y[0]
-    res_i = 1
+    x_res = [x[0]]
+    y_res = [y[0]]
 
     # Initialize base point and slope window
-    xy_base = (x[0], y[0])
-    slope_window = tolerance_slope_window(xy_base, (x[1], y[1]), tol)
+    base_x = x[0]
+    base_y = y[0]
+    slope_min, slope_max = tolerance_slope_window(base_x, base_y, x[1], y[1], tol)
 
     cur_i = 2
     while cur_i < len(x):
-        seg_start = (x[cur_i - 1], y[cur_i - 1])
-        seg_end = (x[cur_i], y[cur_i])
+        seg_end_x = x[cur_i]
+        seg_end_y = y[cur_i]
 
-        # Compute candidate result points by looking if edges of current base cone
-        # intersect with current segment
+        # Compute candidate result point by looking if any edge of current base
+        # cone intersect current segment
         xy_cand = coneedges_segment_intersection(
-            xy_base, slope_window, seg_start, seg_end
+            base_x = base_x,
+            base_y = base_y,
+            slope_min = slope_min,
+            slope_max = slope_max,
+            seg1_x = x[cur_i - 1],
+            seg1_y = y[cur_i - 1],
+            seg2_x = seg_end_x,
+            seg2_y = seg_end_y,
         )
-        xy_not_none = [xy for xy in xy_cand if xy is not None]
 
-        if len(xy_not_none) > 0:
-            # If there is several intersections, take the left one (which
-            # probably shouldn't be possible; added just in case)
-            x_new, y_new = min(xy_not_none, key=lambda xy: xy[0])
-
+        if xy_cand is None:
+            # Update slope window by using intersection of current slope window
+            # and slope window of segment end. Intersection is used because in
+            # order to maintain maximum error within tolerance rays should pass
+            # inside both current 2d cone and 2d cone defined by end of current
+            # segment
+            seg_end_slope_min, seg_end_slope_max = tolerance_slope_window(
+                base_x, base_y, seg_end_x, seg_end_y, tol
+            )
+            slope_min, slope_max = intersect_inervals(
+                slope_min, slope_max, seg_end_slope_min, seg_end_slope_max
+            )
+        else:
             # Write new point
-            x_res[res_i], y_res[res_i] = x_new, y_new
-            res_i += 1
+            x_res.append(xy_cand[0])
+            y_res.append(xy_cand[1])
 
             # Update base point
-            xy_base = (x_new, y_new)
+            base_x, base_y = xy_cand
 
             # Update slope window
             ## If new point is exactly at the end of the current segment,
             ## move one segment further and use its end to compute slope window
-            if xy_base[0] == seg_end[0]:
+            if base_x == seg_end_x:
                 cur_i += 1
                 if cur_i >= len(x):
                     break
@@ -164,112 +174,29 @@ def regrid_maxtol(x, y, tol=1e-3):
             # End of the "current" segment (either this iteration's current
             # segment or next segment in case `xy_base` is moved to current
             # segment end) now defines slope window
-            slope_window = tolerance_slope_window(xy_base, (x[cur_i], y[cur_i]), tol)
-        else:
-            # Update slope window by using intersection of current slope window
-            # and slope window of segment end. Intersection is used because in
-            # order to maintain maximum error within tolerance rays should pass
-            # inside both current 2d cone and 2d cone defined by end of current
-            # segment
-            seg_end_slope_window = tolerance_slope_window(xy_base, seg_end, tol)
-            slope_window = intersect_inervals(slope_window, seg_end_slope_window)
+            slope_min, slope_max = tolerance_slope_window(
+                base_x, base_y, x[cur_i], y[cur_i], tol
+            )
 
         cur_i += 1
 
-    # If the last point wasn't added (most of the times), add it
-    if x_res[res_i - 1] != x_res[-1]:
-        x_res[res_i], y_res[res_i] = x[-1], y[-1]
-        res_i += 1
+    # If the last point wasn't added (as is most of the times), add it
+    if x_res[-1] != x[-1]:
+        x_res.append(x[-1])
+        y_res.append(y[-1])
 
-    if res_i == len(x):
+    if len(x_res) == len(x):
         # If output has the same number of points as input, return input (as it
-        # is always at least not worse)
+        # is always a better choice)
         return x, y
     else:
-        return x_res[:res_i], y_res[:res_i]
+        return np.array(x_res), np.array(y_res)
 
 
 def trapez_integral(x, y):
     """ Compute integral with trapezoidal formula.
     """
     return np.sum(0.5 * np.diff(x) * (y[:-1] + y[1:]))
-
-
-def cons_cos(x, y):
-    """Compute cosine of consecutive vectors
-
-    Xy-grid, created with `x` and `y`, defines a sequence of vectors:
-    `(x[i]-x[i-1]; y[i]-y[i-1])`. This function computes cosine of angles
-    between consecutive pairs between them.  It is assumed that `x` is sorted
-    increasingly, but no checks is done.
-
-    Inputs `x` and `y` should be the same length with at least 3 elements.
-
-    Parameters
-    ----------
-    x : numpy numeric array
-        It is assumed that it is sorted increasingly.
-    y : numpy numeric array
-
-    Returns
-    -------
-    cos : numpy numeric array with length `len(x)-2`
-        Cosine of right-aligned vectors, defined by xy-grid
-
-    Examples
-    --------
-    >>> cons_cos(np.arange(6), np.array([0, 1, 1, 0, 1, 2]))
-    array([0.70710678, 0.70710678, 0.        , 1.        ])
-    """
-
-    def dot_prod(vec_1, vec_2):
-        return vec_1[0] * vec_2[0] + vec_1[1] * vec_2[1]
-
-    vec_x = np.diff(x)
-    vec_y = np.diff(y)
-
-    vec_left = (vec_x[:-1], vec_y[:-1])
-    vec_right = (vec_x[1:], vec_y[1:])
-
-    res = dot_prod(vec_left, vec_right) / np.sqrt(
-        dot_prod(vec_left, vec_left) * dot_prod(vec_right, vec_right)
-    )
-
-    return res
-
-
-def sharpness(x, y):
-    """Compute sharpness of piecewise-linear vertices
-
-    Here "sharpness" is a number between 0 and 1, where 0 represents that point
-    lies on a straight line and 1 - that it is an edge of extreme peak.
-
-    It is assumed that `x` and `y` have the same length. First and last elements
-    by default have sharpness of 0.
-
-    Parameters
-    ----------
-    x : numpy numeric array
-        It is assumed that it is sorted increasingly.
-    y : numpy numeric array
-
-    Returns
-    -------
-    sharpness: numpy array of the same length as `x` and `y`.
-
-    Examples
-    --------
-    >>> sharpness(np.arange(6), np.array([0, 1, 1, 0, 1, 2]))
-    array([0.        , 0.14644661, 0.14644661, 0.5       , 0.        ,
-           0.        ])
-    """
-    # Cosine between consecutive vectors has value `1` if vectors are colinear
-    # (i.e. if their shared point lies on a straight line) and `-1` if they
-    # aligned but have opposite directions (i.e. shared point is an extreme
-    # peak)
-    vec_cos = cons_cos(x, y)
-
-    return np.concatenate([[0], 0.5 * (1 - vec_cos), [0]])
 
 
 def abs_curvature(x, y):
@@ -367,6 +294,7 @@ y = np.interp(x, x_base, y_base)
 # Regridding
 x_maxtol, y_maxtol = regrid_maxtol(x, y, tol=1e-3)
 n_grid_new = len(x_maxtol)
+print(n_grid_new)
 x_equi, y_equi = regrid_equidist(x, y, n_grid_new)
 x_curv, y_curv = regrid_curvature(x, y, n_grid_new)
 x_optim, y_optim = regrid_optimize(x, y, n_grid_new)
@@ -398,19 +326,3 @@ ax.plot(x_equi, y_equi, color="red", marker="o")
 ax.plot(x_curv, y_curv, color="green", marker="o")
 ax.plot(x_optim, y_optim, color="blue", marker="o")
 ax.plot(x_maxtol, y_maxtol, color="magenta", marker="o")
-
-# Plot absolute and regressed-to-mean curvatures
-abs_curv = abs_curvature(x, y)
-abs_curv_regr = regress_to_mean(x, abs_curv, alpha=0.5)
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-ax.plot(x, abs_curv, color="blue")
-ax.plot(x, abs_curv_regr, color="green")
-
-# Study effect of "regress-to-mean" parameter on approximation quality
-alpha_grid = np.linspace(0, 1, 11)
-[
-    dist_grid((x, y), regrid_curvature(x, y, n_grid_new, alpha=a), method="maxabs")
-    for a in alpha_grid
-]
