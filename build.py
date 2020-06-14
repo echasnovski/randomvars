2393fffe1c67e5c8bce5794685ee1efc07e0e149
echasnovski <evgeni.chasnovski@gmail.com>
2020-06-14 16:38:00 +0300

    Update 'build.py'.

--- build.py
+++ build.py
@@ -3,13 +3,11 @@ from Cython.Build import cythonize
 import numpy
 
 
-extensions = cythonize(
-    "randomvars/downgrid_maxtol.pyx",
-    include_path=[numpy.get_include()],
-    language_level=3,
-)
+extensions = cythonize("randomvars/downgrid_maxtol.pyx")
 
 
 def build(setup_kwargs):
     """Build Cython modules"""
-    setup_kwargs.update({"ext_modules": extensions})
+    setup_kwargs.update(
+        {"ext_modules": extensions, "include_dirs": [numpy.get_include()]}
+    )
