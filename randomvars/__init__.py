f6544749e14ed8a7a0902a1eba115b3cb786e227
e.chasnovski <e.chasnovski@simcord.com>
2021-02-18 16:57:45 +0200

    Implement dynamic option registration.

--- randomvars/__init__.py
+++ randomvars/__init__.py
@@ -1,7 +1,7 @@
 """ randomvars: Work with Random Variables
 """
 
-from .options import config, _uses_options, _option_desc
+from .options import config, _uses_options
 from ._random import Rand
 from ._boolean import Bool
 from ._continuous import Cont
@@ -10,8 +10,5 @@ from ._mixture import Mixt
 from .downgrid_maxtol import downgrid_maxtol
 
 # Update `config` with registered options
-option_desc = _uses_options.update_option_desc(_option_desc)
-config.__doc__ = config.__doc__ + "\n".join(option_desc.values())
-del option_desc
+config.__doc__ = config.__doc__ + _uses_options.get_option_desc()
 del _uses_options
-del _option_desc
